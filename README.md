81f723a277d45ed3b761563de8cc8c00fd941040
hemlok <adam.seckel@gmail.com>
2019-08-17 17:04:52 +0100

    refactor to typescript

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # emotion-box 📦
 
-Use `react-emotion` css-in-js to compose your react app flexbox layouts with higher order components! 
+Use `react-emotion` css-in-js to compose your react app flexbox layouts with higher order components!
 
 _I've been copy-pasting some version of this file into every React project I've worked on for the past year... If you're looking for a more fully featured layout library, check out the brilliant https://github.com/jxnblk/styled-system_
 
 supports an 🚨 *opinionated* 🚨 subset of flexbox apis
 
 ## Install
+
 ```
 yarn add emotion-box
 ```
+
 ```
-import Box, {Row, Column} from 'emotion-box'
+import Box, {Row, Column, Props as BoxProps} from 'emotion-box'
 ```
 
 ## Usage
+
 ```
 <Row justify="start" align="start"> ... </Row>
 <Column> ... </Column>
--- index.ts
+++ index.ts
@@ -6,17 +6,41 @@ const justifyMap = {
   'space-between': 'space-between',
   'space-around': 'space-around',
   center: 'center',
+  'space-evenly': 'space-evenly',
 }
 
 const alignMap = {
-  ...justifyMap,
+  start: 'flex-start',
+  end: 'flex-end',
+  center: 'center',
+  baseline: 'baseline',
   stretch: 'stretch',
 }
 
-const justifyContent = ({ justify = 'center' }) => justifyMap[justify]
-const alignItems = ({ align = 'center' }) => alignMap[align]
+const justifyContent = ({ justify = 'center' }: { justify: Justify }) =>
+  justifyMap[justify]
 
-const Box = styled('div')`
+const alignItems = ({ align = 'center' }: { align: Align }) => alignMap[align]
+
+type Justify =
+  | 'start'
+  | 'end'
+  | 'center'
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly'
+
+type Align = 'start' | 'end' | 'center' | 'baseline' | 'stretch'
+
+export interface Props {
+  wrap: boolean
+  grow: boolean
+  inline: boolean
+  justify: Justify
+  align: Align
+}
+
+const Box = styled('div')<Props>`
   display: flex;
   justify-content: ${justifyContent};
   align-items: ${alignItems};
